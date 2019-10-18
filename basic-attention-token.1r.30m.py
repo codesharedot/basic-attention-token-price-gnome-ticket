2c8431154ba319341a7221ea119679d9c6c116f4
Djalil Dreamski <32184973+dreamski21@users.noreply.github.com>
2019-10-18 16:50:31 +0100

    Tidy up the code
    - Remove unused libraries
    - Nice-ify the output (use the symbol BAT instead of the full name + move 'last checked' to the dropdown)
    - Add clearer steps (e.g. "make executable")
    - Rename the script and some variables

---
+++ basic-attention-token.1r.30m.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# How to use:
+#  - Make sure the Argos shell extension (https://github.com/p-e-w/argos) is installed.
+#  - Copy the `basic-attention-token.1r.30m.py` file to ~/.config/argos/
+#  - Make the file executable
+#  - Aaand that's it!
+
+import requests
+from datetime import datetime
+
+def get_price():
+    data = requests.get("https://api.coinmarketcap.com/v1/ticker/basic-attention-token/")
+    price_str = data.json()[0]["price_usd"]
+    price_trimmed = "{0:.2f}".format(float(price_str))
+    price = float(price_trimmed)
+    return price
+
+print("BAT ${usd} | iconName=invest-applet".format(usd=get_price()))
+print("---")
+print("Last checked: " + datetime.now().strftime("%H:%M:%S"))
